25293786fdd418a4c49b0073275deeb591f48a83
Amira Abdel-fattah <amiraelmezien@gmail.com>
2026-05-25 18:47:16 +0300

    feat(splash): Implement HRX App Logo

---
+++ lib/features/splash/widgets/hrx_logo.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:hrx_app/core/utils/app_colors.dart';
+import 'package:hrx_app/core/utils/app_styles.dart';
+
+class HrxLogo extends StatelessWidget {
+  final double fontSize;
+
+  const HrxLogo({super.key, this.fontSize = 36});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          "Performix",
+          style: AppStyles.bold32PrimaryDark().copyWith(
+            fontSize: fontSize,
+            fontWeight: FontWeight.w500,
+            letterSpacing: -0.5,
+            color: AppColors.primaryColor,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: fontSize * .33),
+          child: Text(
+            "|",
+            style: AppStyles.light18HintText().copyWith(
+              color: AppColors.primaryColor.withOpacity(0.3),
+              fontSize: fontSize * .88,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "HR",
+              style: AppStyles.bold32PrimaryDark().copyWith(
+                fontSize: fontSize,
+                color: AppColors.primaryColor,
+              ),
+            ),
+            Text(
+              "X",
+              style: AppStyles.bold32PrimaryDark().copyWith(
+                fontSize: fontSize,
+                color: AppColors.accentColor,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
