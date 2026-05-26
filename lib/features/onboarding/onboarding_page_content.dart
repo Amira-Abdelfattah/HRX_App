46b14605088a24abc07fb2e7e06f265e3b5a3cb2
AmiraAbdel-fattah <amiraelmezien@gmail.com>
2026-05-26 11:21:47 +0300

    refactor: update onboarding page content .

--- lib/features/onboarding/onboarding_page_content.dart
+++ lib/features/onboarding/onboarding_page_content.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../core/utils/app_colors.dart';
-import '../../../../core/utils/app_styles.dart';
+import '../../core/utils/app_colors.dart';
+import '../../core/utils/app_styles.dart';
 import 'onboarding_model.dart';
 
 class OnboardingPageContent extends StatelessWidget {
@@ -12,64 +12,57 @@ class OnboardingPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          height: 300.h,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(24.r),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.03),
-                blurRadius: 20.r,
-                offset: Offset(0, 10.h),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 100.h),
+          Container(
+            height: 350.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(24.r),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 20.r,
+                  offset: Offset(0, 10.h),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(24.r),
+              child: Image.asset(
+                pageModel.imagePath,
+                fit: BoxFit.cover,
               ),
-            ],
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(24.r),
-            child: Image.asset(
-              pageModel.imagePath,
-              fit: BoxFit.contain,
             ),
           ),
-        ),
-        SizedBox(height: 40.h),
-        RichText(
-          textAlign: TextAlign.center,
-          text: TextSpan(
-            children: [
-              TextSpan(
-                text: pageModel.title,
-                style: AppStyles.semi24PrimaryDark().copyWith(
-                  fontSize: 30.sp,
-                  height: 1.3,
-                ),
-              ),
-              TextSpan(
-                text: pageModel.highlightWord,
-                style: AppStyles.semi24PrimaryDark().copyWith(
-                  fontSize: 30.sp,
-                  color: AppColors.accentDarkColor,
-                  height: 1.3,
-                ),
-              ),
-            ],
+          SizedBox(height: 48.h),
+          Text(
+            pageModel.title,
+            textAlign: TextAlign.center,
+            style: AppStyles.semi24PrimaryDark().copyWith(
+              fontSize: 26.sp,
+              color: AppColors.primaryColor,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        SizedBox(height: 20.h),
-        Text(
-          pageModel.description,
-          textAlign: TextAlign.center,
-          style: AppStyles.regular14Grey().copyWith(
-            fontSize: 14.sp,
-            color: AppColors.textBodyColor.withOpacity(0.85),
-            height: 1.6,
+          SizedBox(height: 16.h),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Text(
+              pageModel.description,
+              textAlign: TextAlign.center,
+              style: AppStyles.regular14Grey().copyWith(
+                fontSize: 16.sp,
+                color: AppColors.textBodyColor,
+                height: 1.5,
+              ),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
