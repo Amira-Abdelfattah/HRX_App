393697cb9003922a9db198f6780eb1f264f4e56e
AmiraAbdel-fattah <amiraelmezien@gmail.com>
2026-05-26 11:05:54 +0300

    refactor: enhance onboarding page content .

--- lib/features/onboarding/onboarding_page_content.dart
+++ lib/features/onboarding/onboarding_page_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/app_styles.dart';
@@ -14,6 +15,28 @@ class OnboardingPageContent extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Container(
+          height: 300.h,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.03),
+                blurRadius: 20.r,
+                offset: Offset(0, 10.h),
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(24.r),
+            child: Image.asset(
+              pageModel.imagePath,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+        SizedBox(height: 40.h),
         RichText(
           textAlign: TextAlign.center,
           text: TextSpan(
@@ -21,14 +44,14 @@ class OnboardingPageContent extends StatelessWidget {
               TextSpan(
                 text: pageModel.title,
                 style: AppStyles.semi24PrimaryDark().copyWith(
-                  fontSize: 30,
+                  fontSize: 30.sp,
                   height: 1.3,
                 ),
               ),
               TextSpan(
                 text: pageModel.highlightWord,
                 style: AppStyles.semi24PrimaryDark().copyWith(
-                  fontSize: 30,
+                  fontSize: 30.sp,
                   color: AppColors.accentDarkColor,
                   height: 1.3,
                 ),
@@ -36,11 +59,12 @@ class OnboardingPageContent extends StatelessWidget {
             ],
           ),
         ),
-        const SizedBox(height: 20),
+        SizedBox(height: 20.h),
         Text(
           pageModel.description,
           textAlign: TextAlign.center,
           style: AppStyles.regular14Grey().copyWith(
+            fontSize: 14.sp,
             color: AppColors.textBodyColor.withOpacity(0.85),
             height: 1.6,
           ),
