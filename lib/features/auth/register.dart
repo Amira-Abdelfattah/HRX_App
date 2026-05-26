1646362059bc3360025f227d954126ef894ed413
AmiraAbdel-fattah <amiraelmezien@gmail.com>
2026-05-26 16:59:48 +0300

    feat: -Implement register screen and enhance global gradient background - Created RegisterScreen with responsive form layout.

--- lib/features/auth/register.dart
+++ lib/features/auth/register.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../core/utils/app_colors.dart';
+import '../../core/utils/app_styles.dart';
+import '../widgets/custom_elevated_button.dart';
+import '../widgets/hrx_gradient_bg.dart';
+import 'widgets/register_footer.dart';
+import 'widgets/register_form.dart';
+import 'widgets/register_header.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: HrxGradientBackground(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20.h),
+                const RegisterHeader(),
+                SizedBox(height: 40.h),
+                const RegisterForm(),
+                SizedBox(height: 30.h),
+                CustomElevatedButton(
+                  text: 'Sign up',
+                  backgroundColor: AppColors.accentColor,
+                  textStyle: AppStyles.semi20Primary().copyWith(
+                    color: AppColors.primaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  onButtonClicked: () {},
+                ),
+                SizedBox(height: 24.h),
+                const RegisterFooter(),
+                SizedBox(height: 40.h),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
