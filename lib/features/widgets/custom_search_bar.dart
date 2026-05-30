e9a5f385f4ed81585f2bd154a62da825444f00b1
AmiraAbdel-fattah <amiraelmezien@gmail.com>
2026-05-30 17:29:51 +0300

    Add Custom Search Bar

---
+++ lib/features/widgets/custom_search_bar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../core/utils/app_colors.dart';
+import '../../core/utils/app_styles.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  final String hintText;
+  final ValueChanged<String>? onChanged;
+
+  const CustomSearchBar({
+    super.key,
+    this.hintText = 'Search employees, tasks...',
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Container(
+      height: 50.h,
+      decoration: BoxDecoration(
+        color: isDark ? AppColors.darkSurfaceLightColor : AppColors.whiteColor,
+        borderRadius: BorderRadius.circular(12.r),
+        border: Border.all(
+          color: isDark ? AppColors.darkBorderColor : AppColors.borderColor,
+          width: 1,
+        ),
+        boxShadow: isDark
+            ? []
+            : [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.03),
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+      ),
+      child: TextField(
+        onChanged: onChanged,
+        style: AppStyles.regular14PrimaryDark(
+          color: isDark ? Colors.white : AppColors.primaryColor,
+        ),
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: AppStyles.regular14SearchHint(),
+          prefixIcon: Icon(
+            Icons.search,
+            size: 22.sp,
+            color: AppColors.textMutedColor,
+          ),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(vertical: 12.h),
+        ),
+      ),
+    );
+  }
+}
